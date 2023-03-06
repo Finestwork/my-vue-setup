69f2a45121883b30a015a4f32631ce05f5122238
Finestwork <Younes.Espiritu@outlook.com>
2023-03-07 00:26:36 +0800

    Fix purgecss index.html source path

--- postcss.config.js
+++ postcss.config.js
@@ -1,37 +1,39 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
+module.exports = ({mode}) => {
+    const CSSNANO_CONFIG =
+        mode === 'production'
+            ? require('cssnano')({preset: 'advanced'})
+            : require('cssnano')({
+                preset: [
+                    'advanced',
+                    {
+                        normalizeWhitespace: false,
+                        discardComments: false,
+                        reduceIdents: false,
+                        zindex: false
+                    }
+                ]
+            });
 
-module.exports = ({ mode }) => {
-  const CSSNANO_CONFIG =
-    mode === 'production'
-      ? require('cssnano')({ preset: 'advanced' })
-      : require('cssnano')({
-          preset: [
-            'advanced',
-            {
-              normalizeWhitespace: false,
-              discardComments: false,
-              reduceIdents: false,
-              zindex: false
-            }
-          ]
+    const PURGE_CSS_CONFIG =
+        require('@fullhuman/postcss-purgecss')({
+            content: [`./index.html`, `./src/**/*.vue`],
+            defaultExtractor(content) {
+                const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
+                return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
+            },
+            safelist: [
+                /-(leave|enter|appear)(|-(to|from|active))$/,
+                /^(?!(|.*?:)cursor-move).+-move$/,
+                /^router-link(|-exact)-active$/,
+                /data-v-.*/
+            ]
         });
-  return {
-    plugins: [
-      CSSNANO_CONFIG,
-      require('autoprefixer'),
-      purgecss({
-        content: [`./public/**/*.html`, `./src/**/*.vue`],
-        defaultExtractor(content) {
-          const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
-          return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
-        },
-        safelist: [
-          /-(leave|enter|appear)(|-(to|from|active))$/,
-          /^(?!(|.*?:)cursor-move).+-move$/,
-          /^router-link(|-exact)-active$/,
-          /data-v-.*/
+
+    return {
+        plugins: [
+            CSSNANO_CONFIG,
+            PURGE_CSS_CONFIG,
+            require('autoprefixer'),
         ]
-      })
-    ]
-  };
+    };
 };
